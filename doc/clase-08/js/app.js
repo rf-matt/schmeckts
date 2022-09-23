996f96db6e018f7baa3ad9c777c773af91f0cf2e
rf-matt <ruizfan@gmail.com>
2022-09-22 22:14:08 -0300

    Rama sin entorno desarrollo React

---
+++ doc/clase-08/js/app.js
@@ -0,0 +1,17 @@
+'use strict'; // 
+
+const ReactElement = React.createElement;
+
+export default class Button extends React.Component {
+    
+    render() {
+        return ReactElement(
+            'button',
+            { },
+            'Super Boton'
+        );
+    }
+}
+
+const domContainer = document.getElementById("myApp");
+ReactDOM.render(ReactElement(Button) , domContainer);
